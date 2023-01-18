e5f41fc3fcc8443380299e43995f23c31f7abc46
Geo <george.nogovitsin@gmail.com>
2023-01-18 19:52:53 +0300

    daily commit

---
+++ day_4.py
@@ -0,0 +1,19 @@
+# question 14
+def question14():
+    line = input('Input: ')
+    upper, lower = 0,0
+    for x in line:
+        if x.isalpha():
+            if x.isupper():
+                upper += 1
+            else:
+                lower += 1
+    print(f'UPPER CASE {upper}\nLOWER CASE {lower}')
+#question14()
+
+
+# question 15
+def question15():
+    a = input('Input an integer: ')
+    print(int(a) + int(a*2) + int(a*3) + int(a*4))
+question15()
