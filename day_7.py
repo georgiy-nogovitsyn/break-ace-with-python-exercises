060d8edd3ae47580335075ae1569e063fea60760
Geo <george.nogovitsin@gmail.com>
2023-01-24 21:26:07 +0300

    daily update

--- day_7.py
+++ day_7.py
@@ -1,4 +1,4 @@
-#question 20
+# question 20
 class DivisibleBy7:
     def func():
         num = int(input('Input: '))
@@ -6,3 +6,26 @@ class DivisibleBy7:
             if x % 7 == 0:
                 print(x)
 #DivisibleBy7.func()
+
+
+# question 21
+def question21():
+    from math import sqrt
+    position = 0, 0
+    while True:
+        step = input('Input: ').split()
+        if not step:
+            break
+        elif step[0] == 'UP':
+            position = position[0] + int(step[-1]), position[1]
+        elif step[0] == 'DOWN':
+            position = position[0] - int(step[-1]), position[1]
+        elif step[0] == 'RIGHT':
+            position = position[0], position[1] + int(step[-1])
+        elif step[0] == 'LEFT':
+            position = position[0], position[1] - int(step[-1])
+    print(round(sqrt((position[0]**2 + position[1]**2))))
+#question21()
+
+
+
