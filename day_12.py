6d2896dfff2c4f7cc2d37b58dc8a45accb650cee
Geo <george.nogovitsin@gmail.com>
2023-01-30 21:11:03 +0300

    daily

---
+++ day_12.py
@@ -0,0 +1,17 @@
+# question 44
+def question44():
+    print(list(map(lambda x: x**2, range(1,21))))
+# question44()
+
+
+# question 45
+class American:
+    @staticmethod
+    def printNatinolity():
+        print('American')
+# American.printNatinolity()
+
+
+# question 46
+class NewYorker(American):
+    pass
