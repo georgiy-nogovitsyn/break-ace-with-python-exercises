9aa019c64129018db1d72a1f980d5a969203e095
Geo <george.nogovitsin@gmail.com>
2023-02-04 17:38:14 +0300

    daily update

---
+++ day_17.py
@@ -0,0 +1,25 @@
+# question 65
+import random
+
+
+def question65():
+    li = [2, 4, 6, 8]
+    for x in li:
+        assert x % 2 == 0
+# question65()
+
+
+# question 66
+def question66():
+    print(eval(input('Input: ')))
+# question66()
+
+
+# question 67 ?
+# will do it later
+
+
+# question 68
+def question68():
+    print(random.random() * 100)
+# question68()
