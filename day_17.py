9d416e8ab595f6b464695ea81f48372c1dd79218
Geo <george.nogovitsin@gmail.com>
2023-02-05 21:24:14 +0300

    daily update

--- day_17.py
+++ day_17.py
@@ -15,11 +15,31 @@ def question66():
 # question66()
 
 
-# question 67 ?
-# will do it later
+# question 67
+def binary_search(arr, num):
+    first = 0
+    last = len(arr) - 1
+    while first <= last:
+        middle = first + (last - first) // 2
+        if arr[middle] == num:
+            return middle
+        elif arr[middle] < num:
+            first = middle + 1
+        else:
+            last = middle - 1
+    return -1
+
+arr = [9, 2, 3, 4, 5, 8, 0, 1]
+print(binary_search(sorted(arr), 2))
 
 
 # question 68
 def question68():
     print(random.random() * 100)
 # question68()
+
+
+# question 69
+def question69():
+    print(random.uniform(5, 95))
+# question69()
