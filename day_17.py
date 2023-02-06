ee6d436a313bd026d11e8d27e059f01a708f1be2
Geo <george.nogovitsin@gmail.com>
2023-02-06 20:38:40 +0300

    daily

--- day_17.py
+++ day_17.py
@@ -29,10 +29,24 @@ def binary_search(arr, num):
             last = middle - 1
     return -1
 
-arr = [9, 2, 3, 4, 5, 8, 0, 1]
-print(binary_search(sorted(arr), 2))
+# arr = [9, 2, 3, 4, 5, 8, 0, 1]
+# print(binary_search(sorted(arr), 2))
 
 
+def req_binary_search(arr, num, first, last):
+    middle = first + (last - first) // 2
+    if arr[middle] == num:
+        return middle
+    elif arr[middle] < num:
+        first = middle + 1
+        return req_binary_search(arr, num, first, last)
+    elif arr[middle] > num:
+        last = middle - 1
+        return req_binary_search(arr, num, first, last)
+    else:
+        return -1
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(req_binary_search(arr, 9, 0, len(arr)))
 # question 68
 def question68():
     print(random.random() * 100)
---
+++ day_18.py
@@ -0,0 +1,27 @@
+# question 70
+def question70():
+    import random
+    print(random.choice([x for x in range(0, 11) if x % 2 == 0]))
+question70()
+
+
+# question 71
+def question71():
+    import random
+    print(random.choice([x for x in range(10, 151) if x % 5 == 0 and x % 7 == 0]))
+question71()
+
+
+# question 72
+def question72():
+    pass
+
+
+# question 73
+def question73():
+    pass
+
+
+# question 74
+def question74():
+    pass
