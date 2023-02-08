13fff6c37cea3f83cd054b7e104470c58a709671
Geo <george.nogovitsin@gmail.com>
2023-02-08 21:49:40 +0300

    daily

--- day_18.py
+++ day_18.py
@@ -1,27 +1,33 @@
+import random
+
+
 # question 70
 def question70():
-    import random
     print(random.choice([x for x in range(0, 11) if x % 2 == 0]))
-question70()
+# question70()
 
 
 # question 71
 def question71():
-    import random
     print(random.choice([x for x in range(10, 151) if x % 5 == 0 and x % 7 == 0]))
-question71()
+# question71()
 
 
 # question 72
 def question72():
-    pass
-
+    li = random.sample(range(100, 201), 5)
+    print(li)
+# question72()
 
 # question 73
 def question73():
-    pass
+    li = random.sample(range(100, 201, 2), 5)
+    print(li)
+# question73()
 
 
 # question 74
 def question74():
-    pass
+    li = random.sample([i for i in range(1, 1001) if i % 35 == 0], 5)
+    print(li)
+# question74()
