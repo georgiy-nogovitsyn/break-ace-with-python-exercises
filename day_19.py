5d4e8f404a8d9a363a4d0c433d87209f6c21a82c
Geo <george.nogovitsin@gmail.com>
2023-02-09 23:53:53 +0300

    daily

---
+++ day_19.py
@@ -0,0 +1,25 @@
+# question 75
+def question75():
+    pass
+
+
+# question 76
+def question76():
+    pass
+
+
+# question 77
+def question77():
+    pass
+
+
+# question 78
+def question78():
+    pass
+
+
+# question 79
+def question79():
+    pass
+
+
