b8762f1f36b729bc152884daa29cca0df5aa9431
Geo <george.nogovitsin@gmail.com>
2023-02-10 13:47:33 +0300

    daily update

--- day_19.py
+++ day_19.py
@@ -1,25 +1,51 @@
+import random
 # question 75
 def question75():
-    pass
+    print(random.randrange(7, 16))
 
 
 # question 76
 def question76():
-    pass
+    import zlib
+    s = 'hello world!hello world!hello world!hello world!'
+    s = bytes(s, 'utf-8')
+    s = zlib.compress(s)
+    print(s)
+    print(zlib.decompress(s))
+# question76()
 
 
 # question 77
+from time import time
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, **kwargs)
+        t2 = time()
+        print(f'took {t2-t1} ms')
+        return result
+    return wrapper
+@performance
 def question77():
-    pass
+    for x in range(100):
+        1 + 1
+# question77()
 
 
 # question 78
 def question78():
-    pass
+    li = [3, 6, 7, 8]
+    random.shuffle(li)
+    print(li)
+# question78()
 
 
 # question 79
 def question79():
-    pass
-
+    li = [['I', 'You'], ['Play', 'Love'], ['Hockey', 'Football']]
+    for x in li[0]:
+        for y in li[1]:
+            for z in li[2]:
+                print(f'{x} {y} {z}')
+# question79()
 
