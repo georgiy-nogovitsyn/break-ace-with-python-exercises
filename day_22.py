ca560b2e441d40f8a844285344d41f6c101fba10
Geo <george.nogovitsin@gmail.com>
2023-02-14 23:57:21 +0300

    daily

---
+++ day_22.py
@@ -0,0 +1,35 @@
+# question 90
+def question90():
+    dic = {}
+    for x in 'abcdefgabc':
+        if dic.get(x) == None:
+            dic[x] = 1
+        else:
+            dic[x] = dic[x] + 1
+    for item in dic.keys():
+        print(f'{item},{dic[item]}')
+
+
+# question 91
+def question91():
+    string = input('Input: ')
+    print(string[::-1])
+# question91()
+
+
+# question 92
+def question92():
+    string = input('Input: ')
+    print(string[::2])
+# question92()
+
+
+# question 93
+def question93():
+    import itertools
+    print(list(itertools.permutations([1, 2, 3])))
+# question93()
+
+
+# question 94
+
